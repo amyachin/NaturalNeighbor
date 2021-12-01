c70667b45970e34bd9f15843e8f143db18fa8997
Tyler Howe <tyler.howe@mksinst.com>
2021-12-01 14:15:55 -0800

    fix comment

--- NaturalNeighbor/Csaps.cs
+++ NaturalNeighbor/Csaps.cs
@@ -23,8 +23,7 @@ namespace NaturalNeighbor
         /// between these two extremes is usually only a rather small range of values
         /// for P and its location strongly depends on the data sites.It is in this
         /// small range that P is chosen when it is not supplied, or when an empty
-        /// P or a negative P is input.
-        /// If P > 1 , the corresponding sol</param>
+        /// P or a negative P is input.</param>
         /// <param name="normalizedSmooth">If True, the smooth parameter is normalized such that results are invariant to xdata range and less sensitive to nonuniformity of weights and xdata clumping</param>
         /// <returns>A Smoothing spline which can be evaluated at any interval</returns>
         public static ISmoothingSpline Compute(
